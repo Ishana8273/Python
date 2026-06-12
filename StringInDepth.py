1aadab4a886f11765ae7e4c41dfec918cf484194
Ishana8273 <malikjaivindra91@gmail.com>
2026-06-12 22:39:59 +0530

    add a file StringInDepth and Initialize some important methods and functions

---
+++ StringInDepth.py
@@ -0,0 +1,35 @@
+chai = "Lemon Chai"
+print(chai)
+
+print(chai[0])
+
+slice_chai = chai[0:5]
+print(slice_chai)
+
+print(chai[-1])
+
+print(chai[-4:-1])
+
+num_list = "0123456789"
+print(num_list[:10])
+
+print(num_list[::2])
+
+print(num_list[3:10:3])
+
+print(chai.upper())
+
+print(chai.lower())
+
+chai_one= "     Ginger Chai     "
+
+print(chai_one.strip())
+
+print(chai_one.lstrip())
+
+print(chai_one.rstrip())
+
+print(chai.replace("Lemon","Masala"))
+
+
+
