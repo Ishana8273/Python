9d45da952f4141386d2dab7c8ba0c5b510a41dcd
Ishana8273 <malikjaivindra91@gmail.com>
2026-06-12 22:22:18 +0530

    add SetDT  and some content to SetDT

---
+++ DataTypes/SetDT.py
@@ -0,0 +1,25 @@
+setOne =  {1,2,3,4,5}
+
+print(setOne)
+
+setOne.add(6)
+
+print(setOne)
+
+setOne.remove(3)
+
+print(setOne)
+
+setTwo = {1,2,4,5,6,7,8}
+
+print(setOne|setTwo)
+print(setOne.union(setTwo))
+
+print(setOne&setTwo)
+print(setOne.intersection(setTwo))
+
+print(setOne-setTwo)
+# set()
+
+
+
