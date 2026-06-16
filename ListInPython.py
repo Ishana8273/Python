b7c2da197ddfb2db0b39c88c4b630b378676e94d
Ishana8273 <malikjaivindra91@gmail.com>
2026-06-16 23:24:18 +0530

    add ways to access, slice and update list in ListInDepth

---
+++ ListInPython.py
@@ -0,0 +1,19 @@
+tea_varities = ["Black Tea", "Green Tea", "Oolong Tea", "White Tea"]
+
+print("List of Tea Varieties:")
+print(tea_varities)
+
+print(tea_varities[0]) # Accessing the first element
+
+print(tea_varities[-1]) # Accessing the last element
+
+print(tea_varities[1:3]) # Slicing the list
+
+print(tea_varities[:2]) # Slicing from the beginning to the second element
+
+print(tea_varities[2:]) # Slicing from the third to the fourth element
+
+tea_varities[3] = "Herbal Tea"
+
+print(tea_varities) # Modifying an element in the list
+
