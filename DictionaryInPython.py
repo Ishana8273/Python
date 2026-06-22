ba7f09a588d7a073cc83c5c608cedd3844384e54
Ishana8273 <malikjaivindra91@gmail.com>
2026-06-22 19:00:03 +0530

    add accessing and iterating methods for Dictionary in DictionaryInPython

---
+++ DictionaryInPython.py
@@ -0,0 +1,24 @@
+chai_types = {"Masala": "Spicy", "Ginger": "Zingy", "Lemon": "Tangy"}
+print(chai_types)
+
+print(chai_types["Masala"] )
+
+print(chai_types.get("Masala"))
+
+print(chai_types.get("Lemonn"))
+
+# print(chai_types["Lemonn"])  gives error
+
+chai_types["Lemonn"] = "Citrusy"
+
+for chai in chai_types:
+    print(chai) 
+
+for chai in chai_types:
+    print(chai, chai_types[chai])
+
+    
+for key,value in  chai_types.items():
+    print(key,value)
+    
+            
